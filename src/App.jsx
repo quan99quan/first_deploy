74407f37e1400445e1cde1abf3a6c6ced7cabd47
quan <duongquan19999@gmail.com>
2023-11-07 16:02:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import UseState from "./assets/hook/useState";
+import UseContext from "./assets/hook/UseContext";
+import UseEffect from "./assets/hook/UseEffect";
+import UseReducer from "./assets/hook/UseReducer";
+import UseMemo from "./assets/hook/UseMemo";
+import UseCallback from "./assets/hook/UseCallback";
+function App() {
+  return (
+    <>
+      {/* <UseState/> */}
+      {/* <UseContext /> */}
+      {/* <UseEffect /> */}
+      {/* <UseReducer/> */}
+      {/* <UseMemo/> */}
+      <UseCallback/>
+    </>
+  );
+}
+
+export default App;
